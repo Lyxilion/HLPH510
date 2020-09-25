240654733e0cbbbe4bbc7834ddf3fdeb453f0e66
Boucher Rubens <boucher.rubens.974@gmail.com>
2020-09-25 22:11:20 +0200

    PEP 8 Corections

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -1,24 +1,25 @@
-while 1 :
-    n=input("\n Quelle question ? (1-8)\n")
-    if n==str(1):
+while 1:
+    n = input("\n Quelle question ? (1-8)\n")
+    if n == str(1):
         print("1. 20 premiers termes de la table de multiplication par 7.")
         for i in range(20):
             print('7x' + str(i) + '=' + str(i * 7))
 
-    elif n==str(2):
+    elif n == str(2):
         print("table de conversion de sommes d'argent :")
         for i in range(15):
             print(str(2 ** i) + ' euro(s) = ' + str(1.65 * 2 ** i) + ' dollar(s)')
 
-    elif n==str(3):
+    elif n == str(3):
         print("2. Suite de 12 nombres dont chaque terme est égal au triple du terme précédent :")
         k = 1
         for i in range(12):
             print(str(i + 1) + " : " + str(k))
             k = k * 3
 
-    elif n==str(4):
+    elif n == str(4):
         print("4. Volume d'un parallélépipède rectangle : ")
+
         def volume(l: float, h: float, p: float):
             """
             Calcule le volume d'un parallelepipede rectangle
@@ -28,12 +29,12 @@ while 1 :
             :return: volume
             """
             return l * h * p
-        l=float(input('Largeur = '))
-        h=float(input('Hauteur = '))
-        p=float(input('Profondeur = '))
-        print(volume(l, h, p))
+        x = float(input('Largeur = '))
+        y = float(input('Hauteur = '))
+        z = float(input('Profondeur = '))
+        print(volume(x, y, z))
 
-    elif n==str(5):
+    elif n == str(5):
         def date(s: int):
             """
             Converti un npmbre de seconde en date
@@ -49,22 +50,22 @@ while 1 :
             print(str(j) + ' jour(s) ' + str(h) + ' heure(s) ' + str(m) + ' minutes ' + str(s) + ' seconde(s)')
         s = int(input("Nombre de seconde :"))
         date(s)
-    elif n==str(6):
+    elif n == str(6):
         print("20 premiers termes de la table de multiplication par 7 et * = multiple de 3 :")
         for i in range(20):
             print(str(i * 7) + '*' if (i * 7 % 3 == 0) else str(i * 7), end=" ")
 
-    elif n==str(7):
+    elif n == str(7):
         k = ""
         for i in range(7):
             k += str(i + 1) + " "
             print(k)
 
-    elif n==str(8):
+    elif n == str(8):
         for i in range(7):
             for j in range(7-i):
-                print(j+1,end =" ")
+                print(j+1, end=" ")
             print("")
-    else :
+    else:
         print('End')
         break
--- TP4/TP4.py
+++ TP4/TP4.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 xmin = 0
 xmax = 3*np.pi/2
 nbx = 20
-nbi = nbx - 1 # nombre d'intervalles
+nbi = nbx - 1  # nombre d'intervalles
 
 x = np.linspace(xmin, xmax, nbx)
 y = np.cos(x)
-plt.plot(x,y,"bo-")
+plt.plot(x, y, "bo-")
 
 integrale = 0
 for i in range(nbi):
     integrale = integrale + (x[i+1]-x[i])*(y[i]+y[i+1])/2
     # dessin du trapeze
-    x_rect = [x[i], x[i], x[i+1], x[i+1], x[i]] # abscisses des sommets
-    y_rect = [0   , y[i], y[i+1]  , 0     , 0   ] # ordonnees des sommets
-    plt.plot(x_rect, y_rect,"r")
+    x_rect = [x[i], x[i], x[i+1], x[i+1], x[i]]  # abscisses des sommets
+    y_rect = [0, y[i], y[i+1], 0, 0]  # ordonnees des sommets
+    plt.plot(x_rect, y_rect, "r")
 print("integrale =", integrale)
 
 plt.show()
