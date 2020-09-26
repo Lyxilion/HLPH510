017734ea00e49f5c8c52dd822e294832348e495b
Boucher Rubens <boucher.rubens.974@gmail.com>
2020-09-26 12:49:16 +0200

    PEP 8 Corections

--- TP3/TP3.py
+++ TP3/TP3.py
@@ -1,42 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 np.random.seed(9)
+
 
 def random_walk():
     """
         Fait une marche aleatoire de 100 pas
     :return:
     """
-    x = np.array([0])
-    for i in range(99) :
-        if np.random.random() < 0.5 :
-            x = np.append(x,np.array([x[i]+1]))
-        else :
-            x = np.append(x,np.array([x[i]-1]))
+    walk = np.array([0])
+    for i in range(99):
+        if np.random.random() < 0.5:
+            walk = np.append(walk, np.array([walk[i]+1]))
+        else:
+            walk = np.append(walk, np.array([walk[i]-1]))
     return x
 
-time = np.linspace(0,100,100)
+
+time = np.linspace(0, 100, 100)
 x = random_walk()
 
-plt.plot(time,x)
+plt.plot(time, x)
 plt.title("Marche aleatoir.")
 plt.xlabel("Temps")
 plt.ylabel("Distance")
 plt.show()
 
-plt.plot(time,x**2)
+plt.plot(time, x**2)
 plt.title("Distance au carré.")
 plt.xlabel("Temps")
 plt.ylabel("Distance")
 plt.show()
 
-for i in range(499):
-    x = np.vstack((x,random_walk()))
+for n in range(499):
+    x = np.vstack((x, random_walk()))
 
-sum = np.sum(x, axis=0)/len(x)
+moy = np.sum(x, axis=0)/len(x)
 
-plt.plot(time,sum)
+plt.plot(time, moy)
 plt.title("Moyenne des 500 marches")
 plt.xlabel("Temps")
 plt.ylabel("Distance")
