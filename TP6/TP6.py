279fae59c208806bb4e32f8e36b6abd5d29c76f2
Boucher Rubens <boucher.rubens.974@gmail.com>
2020-11-14 16:54:26 +0100

    Added TP6.py

---
+++ TP6/TP6.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = 3.5  # Longueur d'un coté du triangle
+x = [0, a, a / 2, 0]  # Calcule des coordonnées des sommets
+y = [0, 0, np.sqrt(a ** 2 - (a ** 2 / 4)), 0]
+
+x_p = 2  # Coordonnées du point arbitraire P
+y_p = 0.5
+
+plt.plot(x, y)  # tracé du triangle et de P
+plt.scatter(x_p, y_p, marker="o", c='red')
+
+x_k = x_p
+y_k = y_p
+for i in range(50):
+    rnd = np.random.randint(1, 4)  # Tirage d'un entier aléatoire entre 1 et 3
+    if rnd == 1:  # Calcule des coordonnées du nouveau point
+        x_k = (x_k + x[0]) / 2
+        y_k = (y_k + y[0]) / 2
+    elif rnd == 2:
+        x_k = (x_k + x[1]) / 2
+        y_k = (y_k + y[1]) / 2
+    else:
+        x_k = (x_k + x[2]) / 2
+        y_k = (y_k + y[2]) / 2
+    plt.scatter(x_k, y_k, marker=".", c='green')  # Tracé du point
+    plt.pause(0.01)
+plt.show()
