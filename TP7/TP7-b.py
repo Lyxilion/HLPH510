cc7555bec8edcefe5e655b7e3433360bb13acb96
Boucher Rubens <boucher.rubens.974@gmail.com>
2020-11-15 09:45:21 +0100

    improved code

--- TP7/TP7-b.py
+++ TP7/TP7-b.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+# Calcule des Coordonnées
 x = np.linspace(-3, 13, 161)
 dt = 0.1
 
@@ -13,10 +14,10 @@ plt.ylim(-1, 1.5)
 
 def animate(i):
     t = i * dt
-    y = 1 / (1 + (x - 4.8*t)**2)
+    y = 1 / (1 + (x - 4.8*t)**2)  # Expression de la fonction
     line.set_data(x, y)
     return line,
 
-
+# Animation
 ani = animation.FuncAnimation(fig, animate,  frames=41, interval=20, repeat=False)
 plt.show()
--- TP7/TP7-c.py
+++ TP7/TP7-c.py
@@ -13,9 +13,17 @@ Y = np.array([y0,y])
 Z = np.array([z,z])
 C = np.angle(Z)
 
+# Affichages des informations du graph
+plt.rcParams.update({'mathtext.default': 'regular' })  # Activer la synthax LateX
+plt.title("y = $e^{-i2 \pi x}  e^{-x^{2}}$")
+plt.xlabel("x")
+plt.ylabel("Module de y")
+
+# Affichage du module
 plt.plot(x, y, "k")
 
+# Affichage de l'argument
 plt.pcolormesh(X, Y, C, shading="gouraud", cmap=plt.cm.hsv, vmin=-np.pi, vmax=np.pi)
-plt.colorbar()
+plt.colorbar().set_label("Argument de y")
 
 plt.show()
