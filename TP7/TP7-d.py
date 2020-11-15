482860e94ef04334e45a90a92c33d37be2baee5d
Boucher Rubens <boucher.rubens.974@gmail.com>
2020-11-15 09:24:20 +0100

    Added TP7-d.py Added TP7-d.png

---
+++ TP7/TP7-d.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Définition des axes
+x = np.linspace(-2, 2, 21)
+y = np.linspace(-7, 7, 29)
+X, Y = np.meshgrid(x, y)
+
+Z = np.log(3*X**2+Y**2+1)  # Calcule de z
+
+# Affichage
+plt.pcolormesh(X, Y, Z, shading='auto')
+plt.colorbar().set_label("f(x,y) = ln(3x² + y² + 1")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Représentation fonction à deux variables")
+
+plt.show()
