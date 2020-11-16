28e25d40b81874a456c798fca221e0f79e4d808f
Boucher Rubens <boucher.rubens.974@gmail.com>
2020-11-16 19:33:04 +0100

    Small code tweaking before submiting code

--- TP5/TP5-b.py
+++ TP5/TP5-b.py
@@ -3,20 +3,20 @@ import numpy as np
 
 """ Points aléatoires dans le coin supérieur droit du carré """
 
-n = 1000  # Nombre de point a tirer
+n = 500  # Nombre de point a tirer
 nc = 0  # Nombre de point dans le cercle
 a = 1.25  # Côté du carré
 
 # Affichage des formes :
 # Le Carré :
-x = np.array([0, a/2, a/2])
-y = np.array([a/2, a/2, 0])
+x = np.array([0, 0, a, a, 0])
+y = np.array([0, a, a, 0, 0])
 plt.plot(x, y)
 
 # Le Cercle :
 theta = np.linspace(0, np.pi/2, 40)
-x = a / 2 * np.cos(theta)
-y = a / 2 * np.sin(theta)
+x = a * np.cos(theta)
+y = a * np.sin(theta)
 plt.plot(x, y)
 
 plt.title("Visualisation des points")
@@ -24,9 +24,9 @@ plt.axis("equal")
 
 for i in range(n):
     # On tire deux nombres aleatoires, un pour x, un pour y entre -a/2 et a/2
-    coords = np.random.uniform(0, a / 2, 2)
+    coords = np.random.uniform(0, a , 2)
     # On verifie si la distance entre le point et l'origine et inferieure au rayon du cercle
-    if np.sqrt(coords[0] ** 2 + coords[1] ** 2) <= a / 2:
+    if np.sqrt(coords[0] ** 2 + coords[1] ** 2) <= a :
         nc += 1  # Si il est dans le cercle, on augmente nc
         plt.scatter(coords[0], coords[1], marker=".", c='green')  # si il est dans le cercle => vert
     else:
